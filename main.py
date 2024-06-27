cf117b3cb43131fbe61fb040ebdf5035de9642d6
mhensberg2003 <mhensberg2003@gmail.com>
2024-06-27 11:39:22 +0200

    improved count functionality and added seperate functions to handle tasks

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
-    book = "books/frankenstein.txt"
-    with open (book) as f:
-        count = 0
-        file_contents = f.read()
-        words = file_contents.split()
-        for word in words:
-            count += 1
-        print(count)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 main()
